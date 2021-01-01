3595c8a67dcd632747a84f2e289cccadbf268e99
danidee <danielle@Danielles-MacBook-Pro.local>
2021-01-01 16:47:11 -0500

    First commit of the project

--- app.py
+++ app.py
@@ -8,17 +8,19 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+
 @app.route("/")
 def index():
-   mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
 
 @app.route("/scrape")
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful!"
 
-   if __name__ == "__main__":
-   app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
